ded92cae84535844116ee5781a5e4cf20623ed8a
Michael Arnaldi <arnaldimichael@gmail.com>
2022-12-22 16:13:29 +0000

    add example code

---
+++ examples/subscription.ts
@@ -0,0 +1,47 @@
+import * as Effect from "@effect/io/Effect"
+import * as Stream from "@effect/stream/Stream"
+import * as Chunk from "@fp-ts/data/Chunk"
+import { pipe } from "@fp-ts/data/Function"
+
+import * as Fiber from "@effect/io/Fiber"
+import * as SubscriptionRef from "@effect/stream/SubscriptionRef"
+import * as Equal from "@fp-ts/data/Equal"
+import * as Number from "@fp-ts/data/Number"
+
+const program = Effect.gen(function*($) {
+  console.log("START")
+  const subscriber = (subscriptionRef: SubscriptionRef.SubscriptionRef<number>) =>
+    pipe(
+      Effect.log("STARTED"),
+      Effect.flatMap(() =>
+        pipe(
+          subscriptionRef.changes,
+          Stream.take(20),
+          Stream.runCollect
+        )
+      ),
+      Effect.tap(() => Effect.log("STOPPED"))
+    )
+  const subscriptionRef = yield* $(SubscriptionRef.make(0))
+  const fiber = yield* $(pipe(
+    subscriptionRef,
+    SubscriptionRef.update((n) => n + 1),
+    Effect.forever,
+    Effect.fork
+  ))
+  const result = yield* $(
+    Effect.collectAllPar(
+      Array.from({ length: 2 }, () => subscriber(subscriptionRef))
+    )
+  )
+  yield* $(Fiber.interrupt(fiber))
+  const isSorted = pipe(
+    result,
+    Chunk.every((chunk) => Equal.equals(chunk, pipe(chunk, Chunk.sort(Number.Order))))
+  )
+  return isSorted
+})
+
+Effect.unsafeRunPromiseExit(program).then((exit) => {
+  console.log(exit)
+})
