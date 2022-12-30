029ed36445cc19d438adee779eb617c0438141e8
Michael Arnaldi <arnaldimichael@gmail.com>
2022-12-30 11:52:51 +0000

    use vite plugin with caching

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,18 @@
 /// <reference types="vitest" />
+import { effectPlugin } from "@effect/vite-plugin"
 import path from "path"
 import { defineConfig } from "vite"
-import { tsPlugin } from "./plugins/vitePlugin"
 
 export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  plugins: [tsPlugin({})],
+  plugins: [
+    effectPlugin({
+      babel: {
+        plugins: [["annotate-pure-calls"]],
+        compact: false
+      },
+      tsconfig: "tsconfig.test.json"
+    })
+  ],
   test: {
     include: ["./test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     exclude: ["./test/utils/**/*.ts", "./test/**/*.init.ts"],
